f084956dabf1648ae23877dac76adb495192f55d
Mazen Mahmoud <zoonattack18@gmail.com>
2025-07-09 22:11:10 +0300

    finished the search functinoality with paging aswell as a search page.

---
+++ Netflix-Clone/Models/SearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Netflix_Clone.Data;
+
+namespace Netflix_Clone.Models
+{
+    public class SearchViewModel
+    {
+        public List<Movie> Movies { get; set; }
+        public List<Show> Shows { get; set; }
+        public string Query { get; set; }
+    }
+}
